a5243579a0d7890c524e6c407a729df19f253ed2
Kazuhiko Miyamoto <k.miyamoto.0128@gmail.com>
2017-03-26 15:18:24 +0900

    Initial Commit

---
+++ SwiftPoem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPoem
+//
+//  Created by 宮本一彦 on 2017/03/26.
+//  Copyright © 2017年 宮本一彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
